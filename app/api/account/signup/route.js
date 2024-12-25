77da7d655b66aafcd09e632f0b9b2f70e5234ff2
sakshamwithweb <websaksham39@gmail.com>
2024-12-25 20:01:09 +0530

    verify rules in signup

--- app/api/account/signup/route.js
+++ app/api/account/signup/route.js
@@ -3,11 +3,32 @@ import connectDb from "@/lib/mongoose"
 import { rateLimit } from "@/lib/rateLimit";
 import { NextResponse } from "next/server"
 
+
+function isValidEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+
 export async function POST(params) {
     try {
         const isAllowed = await rateLimit(params, "signup");
         if (!isAllowed) return NextResponse.json({ success: false, message: "Attempt is over, Try 5 minutes later" });
         const { email, password, fullName, userName } = await params.json()
+        if (
+            !userName.includes(" ") &&
+            userName.length >= 8 &&
+            userName.length <= 20 &&
+            /^[a-zA-Z0-9.]+$/.test(userName) &&
+            isValidEmail(email) &&
+            password.length >= 8 &&
+            password.length <= 30 &&
+            fullName.length >= 5 &&
+            fullName.length <= 30
+        );
+        else {
+            return NextResponse.json({ success: false, message: "any rule is avoided" })
+        }
         await connectDb()
         const checkUser = await User.findOne({ email: email })
         if (checkUser) return NextResponse.json({ success: false, message: "Account with same email id exists" })
@@ -18,7 +39,6 @@ export async function POST(params) {
             userName
         })
         await newUser.save()
-        //console.log(email, password, fullName, userName)
         return NextResponse.json({ success: true })
     } catch (error) {
         return NextResponse.json({ success: false, message: "Something went wrong" })
