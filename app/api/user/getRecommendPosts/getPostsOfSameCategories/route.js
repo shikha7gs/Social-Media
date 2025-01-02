21af3b9babb2f3df8d62885589292216998d6b85
sakshamwithweb <websaksham39@gmail.com>
2025-01-02 07:03:06 +0530

    Same category was not filtering, rather all posts were comming

--- app/api/user/getRecommendPosts/getPostsOfSameCategories/route.js
+++ app/api/user/getRecommendPosts/getPostsOfSameCategories/route.js
@@ -4,25 +4,25 @@ import connectDb from "@/lib/mongoose"
 import { rateLimit } from "@/lib/rateLimit";
 import { NextResponse } from "next/server"
 
-async function categorizePosts(categories, followings,userName) {
+async function categorizePosts(categories, followings, userName) {
     const followingPost = [];
     const restPost = [];
-
-    const fetchAndCategorizePosts = async (userName) => {
+    // Call for every new category
+    const fetchAndCategorizePosts = async (userName, category) => {
         const postArr = await Post.find({});
         postArr.forEach((post) => {
             const targetArray = followings.includes(post.userName) ? followingPost : restPost;
             post.posts.forEach((item) => {
-                if (!item.viewers.includes(userName)){
+                // Just push the post which has same category as category and it is new for user
+                if (item.category == category && !item.viewers.includes(userName)) {
                     targetArray.push(item)
                 }
             });
-            // If viewers arr has userName of viewer? drop it or else no...
         });
     };
 
     await Promise.all(
-        Object.keys(categories).map(() => fetchAndCategorizePosts(userName))
+        Object.keys(categories).map((category) => fetchAndCategorizePosts(userName, category)) // It is called for every categories, ai_ml
     );
 
     return { followingPost, restPost };
@@ -32,7 +32,7 @@ export async function POST(req) {
     try {
         const isAllowed = await rateLimit(req, "checkSession");
         if (!isAllowed) return NextResponse.json({ success: false, message: "Too many requests, try after 5 minutes" });
-        const { categories, followings, id ,userName} = await req.json()
+        const { categories, followings, id, userName } = await req.json()
         const token = req.headers.get('authorization')?.split(' ')[1]
         if (!token || !id) return NextResponse.json({ success: false, message: "Token is required" })
         const validateToken = await validateJWT(token, id)
@@ -40,7 +40,7 @@ export async function POST(req) {
 
         await connectDb()
         // Task: Find same categories post in every followings account then same categories in rest accounts
-        const ans = await categorizePosts(categories, followings,userName)
+        const ans = await categorizePosts(categories, followings, userName)
         const combinedArray = [...ans.followingPost, ...ans.restPost];
         let selectedPosts = []
         combinedArray.map((i) => {
